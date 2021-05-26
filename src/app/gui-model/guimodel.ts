984cd59bb420c3a1a24431133e93e697e9ab4f3d
LazarBasaric <lazarbasaric00@gmail.com>
2021-05-26 01:00:31 +0000

    text corrected

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Lazars Friend Tracker",
+            "title": "Lazar's Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
